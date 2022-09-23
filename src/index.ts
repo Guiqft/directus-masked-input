8214f1e174a6179fbf95e110f5e3a309fc640a4b
Guilherme de Oliveira <guilherme.oliveira@luizalabs.com>
2022-09-23 13:28:35 -0300

    feat: write new interface options and add brl currency mask

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,91 @@
 import MaskedInput from "./MaskedInput.vue"
-import MaskOptions from "./MaskOptions.vue"
-import PreviewSVG from "./preview.svg"
+// import PreviewSVG from "./preview.svg?raw"
 
 export default {
     id: "masked-input",
     name: "Entrada com Máscara",
     description:
-        "Interface para um texto mascarado com predefinições para: CPNF/CNPJ, Código de Inscriçao e Telefone",
+        "Interface para um texto mascarado com predefinições para: CPF, CNPJ, Código de Inscriçao e Telefone",
     icon: "box",
     types: ["string"],
-    component: MaskedInput,
-    options: MaskOptions,
     group: "standard",
-    preview: PreviewSVG,
+    // preview: PreviewSVG,
+    component: MaskedInput,
+    options: [
+        {
+            field: "maskType",
+            name: "Tipo de Máscara",
+            type: "string",
+            schema: {
+                default_value: "cpf",
+            },
+            meta: {
+                required: true,
+                width: "full",
+                interface: "select-dropdown",
+                options: {
+                    choices: [
+                        { text: "CPF", value: "cpf" },
+                        { text: "CNPJ", value: "cnpj" },
+                        {
+                            text: "Código de Inscrição",
+                            value: "inscription_code",
+                        },
+                        { text: "Telefone", value: "telephone" },
+                        { text: "Dinheiro", value: "currency" },
+                        { text: "Customizada", value: "custom" },
+                    ],
+                },
+            },
+        },
+        {
+            field: "validate",
+            type: "boolean",
+            name: "Validar Input",
+            meta: {
+                width: "half",
+                interface: "boolean",
+                conditions: [
+                    {
+                        rule: {
+                            maskType: {
+                                _eq: "currency",
+                            },
+                        },
+                        hidden: true,
+                    },
+                ],
+            },
+            schema: {
+                default_value: true,
+            },
+        },
+        {
+            field: "customPattern",
+            type: "string",
+            name: "Padrão customizado",
+            meta: {
+                interface: "input",
+                width: "half",
+                note: 'Múltiplas máscaras devem ser separadas por vírgula e espaço. <br />\
+                    Ex: (00) 0000-0000, (00) 00000-0000 <br />\
+                    Mais informações em: <a href="https://imask.js.org/guide.html#masked-pattern">iMask Pattern</a>',
+                options: {
+                    placeholder: "Sua máscara customizada",
+                },
+                hidden: true,
+                conditions: [
+                    {
+                        rule: {
+                            maskType: {
+                                _eq: "custom",
+                            },
+                        },
+                        hidden: false,
+                        required: true,
+                    },
+                ],
+            },
+        },
+    ],
 }
